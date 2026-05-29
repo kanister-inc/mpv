4935231f2dfeb7006b02ab782a51ce45fd75f2c2
1TShelby <romafedin231@gmail.com>
2026-05-29 11:22:32 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev
 export default defineConfig({
   plugins: [react()],
-  base: './', // <-- ЭТО КРИТИЧЕСКИ ВАЖНО ДЛЯ GITHUB PAGES
+  base: '/mpv/', // <-- ДОБАВЬТЕ ЭТУ СТРОКУ (имя вашего репозитория со слэшами)
 })
